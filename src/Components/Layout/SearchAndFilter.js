96e9f04f6f2ee0935f99eaa285c22ebedb29f9e4
Zybartas Ringys <developerzr92@gmail.com>
2022-09-29 22:21:09 +0300

    Search

--- src/Components/Layout/SearchAndFilter.js
+++ src/Components/Layout/SearchAndFilter.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 function SearchAndFilter() {
   return (
-    <div className='container mt-3'>
-      <div className='row d-flex '>
-        <div className='col-12 border'>
-          <h1>Search</h1>
+    <div className="container mt-3">
+      <div className="row d-flex ">
+        <div className="col-12 col-xl-5 ">
+          <div class="input-group country-input mb-3 border">
+            <div className="icon">
+              <FaSearch />
+            </div>
+
+            <input
+              type="text"
+              class="form-control "
+              placeholder="Search for a country..."
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            ></input>
+          </div>
         </div>
-        <div className='col-6 mt-4 float-left border'>
-          <h1>Filter</h1>
+        <div className="col-6 col-xl-2 float-left border">
+          <h5>Filter</h5>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SearchAndFilter
+export default SearchAndFilter;
