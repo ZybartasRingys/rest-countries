0034973d5cbb464b97bd3f4461bda4e49f81756e
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-10-04 10:16:33 +0300

    filter functionality

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,55 +1,39 @@
-import React from "react";
-import "../scss/card.scss";
-import { useContext, useState } from "react";
-import { Context } from "../Context/StateContext";
+import React from 'react'
+import '../scss/card.scss'
+import { useContext } from 'react'
+import { Context } from '../Context/StateContext'
 
 function Card() {
-  const { countries, setCountries, searchInput, setSearchInput, searchParam } =
-    useContext(Context);
-
-  function search(items) {
-    return items.filter((item) => {
-      return searchParam.some((newItem) => {
-        return (
-          /* Converting the item to a string, then to lowercase, then it is checking if the searchInput
-         is in the item. */
-          item[newItem]
-            .toString()
-            .toLowerCase()
-            .indexOf(searchInput.toLowerCase()) > -1
-        );
-      });
-    });
-  }
+  const { countries, search } = useContext(Context)
 
   return search(countries).map((country) => {
-    const { name, population, region, capital, flag, numericCode } = country;
+    const { name, population, region, capital, flag, numericCode } = country
 
     return (
       <div
-        className="col-12 col-md-6 col-xl-3 d-flex justify-content-center"
+        className='col-12 col-md-6 col-xl-3 d-flex justify-content-center'
         key={numericCode}
       >
-        <div className="card shadow  mb-5 bg-white rounded">
-          <div className="card-image">
-            <img className="image" src={flag} alt="flag" />
+        <div className='card shadow  mb-5 bg-white rounded'>
+          <div className='card-image'>
+            <img className='image' src={flag} alt='flag' />
           </div>
-          <div className="card-body text-left">
-            <h5 className="card-title mb-4">{name}</h5>
-            <p className="card-text">
+          <div className='card-body text-left'>
+            <h5 className='card-title mb-4'>{name}</h5>
+            <p className='card-text'>
               <span>Population:</span> {population}
             </p>
-            <p className="card-text">
+            <p className='card-text'>
               <span>Region:</span> {region}
             </p>
-            <p className="card-text">
+            <p className='card-text'>
               <span>Capital:</span> {capital}
             </p>
           </div>
         </div>
       </div>
-    );
-  });
+    )
+  })
 }
 
-export default Card;
+export default Card
--- src/Context/StateContext.js
+++ src/Context/StateContext.js
@@ -1,23 +1,38 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
-import Card from "../Components/Card";
+import React, { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
-export const Context = createContext();
+export const Context = createContext()
 
 export const ContextProvider = ({ children }) => {
-  const [countries, setCountries] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
-  const [searchParam] = useState(["capital", "name"]);
+  const [countries, setCountries] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  const [searchParam] = useState(['capital', 'name'])
 
   /*  Country data endpoint  */
   const baseURL =
-    "https://restcountries.com/v2/alpha?codes=DEU,USA,BRA,ISL,AFG,ALA,ALB,DZA";
+    'https://restcountries.com/v2/alpha?codes=DEU,USA,BRA,ISL,AFG,ALA,ALB,DZA'
 
+  /* A hook that is used to fetch data. */
   useEffect(() => {
     axios.get(baseURL).then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
+      setCountries(response.data)
+    })
+  }, [])
+
+  function search(items) {
+    return items.filter((item) => {
+      return searchParam.some((newItem) => {
+        return (
+          /* Converting the item to a string, then to lowercase, then it is checking if the searchInput
+         is in the item. */
+          item[newItem]
+            .toString()
+            .toLowerCase()
+            .indexOf(searchInput.toLowerCase()) > -1
+        )
+      })
+    })
+  }
 
   return (
     <Context.Provider
@@ -26,10 +41,10 @@ export const ContextProvider = ({ children }) => {
         setCountries,
         searchInput,
         setSearchInput,
-        searchParam,
+        search,
       }}
     >
       {children}
     </Context.Provider>
-  );
-};
+  )
+}
