c5f6ff230c0a821bfb98bce3cac6cccdc592320b
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-10-07 16:12:18 +0300

    card detail UI

--- src/Components/CardDetail.jsx
+++ src/Components/CardDetail.jsx
@@ -3,22 +3,74 @@ import '../scss/card.scss'
 import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { Context } from '../Context/StateContext'
+import { FaBorderStyle } from 'react-icons/fa'
 
 function CardDetail() {
   const { countries } = useContext(Context)
   const { id } = useParams()
   return countries.map((country) => {
-    const { flag, name, numericCode, topLevelDomain } = country
+    {
+      console.log(country)
+    }
+    const {
+      flag,
+      name,
+      numericCode,
+      topLevelDomain,
+      population,
+      region,
+      capital,
+      nativeName,
+      subregion,
+      currencies,
+      borders,
+    } = country
     if (id === name) {
       return (
         <div className='container border' key={numericCode}>
           <div className='row'>
             <div className='col-12 border'>
-              <img className='detail-card-image' src={flag} alt={name} />
+              <div className='col-12 border mb-5 mt-3'>
+                <button> back</button>
+              </div>
+              <div className='col-12 mb-5'>
+                <img className='detail-card-image' src={flag} alt={name} />
+              </div>
             </div>
-            <div className='col'></div>
+            <div className='col-12 border'>
+              <div className='col'>
+                <div className='col-12'>
+                  <h2>{name}</h2>
+                  <p>Native name:{nativeName}</p>
+                  <p>Population:{population}</p>
+                  <p>Region:{region}</p>
+                  <p>Sub region:{subregion} </p>
+                  <p>Capital:{capital} </p>
+                </div>
+                <div className='col-12'>
+                  <p>
+                    <span className='bold'>Top Level Domain:</span>
+                    {topLevelDomain}
+                  </p>
+                  <p>
+                    <span className='bold'>
+                      Currencies:{currencies[0].code}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <div className='col-12'>
+                <div className='col'>
+                  <h2>border Co-untries:</h2>
+                </div>
+                <div className='col'>
+                  {borders.map((borderCountry) => {
+                    return <button>{borderCountry}</button>
+                  })}
+                </div>
+              </div>
+            </div>
           </div>
-          <h1>{topLevelDomain}</h1>
         </div>
       )
     }
