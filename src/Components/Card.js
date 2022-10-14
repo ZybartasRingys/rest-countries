6aaae230c620e4431474a52bad0e1feb10e8a0a3
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-10-14 15:14:14 +0300

    cards container

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,33 +1,33 @@
-import React from "react";
-import "../scss/card.scss";
-import { useContext } from "react";
-import { Context } from "../Context/StateContext";
-import { Link } from "react-router-dom";
+import React from 'react'
+import '../scss/card.scss'
+import { useContext } from 'react'
+import { Context } from '../Context/StateContext'
+import { Link } from 'react-router-dom'
 
 function Card() {
-  const { countries, searchInput, filtered } = useContext(Context);
+  const { countries, searchInput, filtered } = useContext(Context)
 
   return (
     <>
       {searchInput.length > 0 ? (
-        <div className="col-12 col-md-6 col-xl-3 d-flex justify-content-center ">
+        <div className='container d-flex'>
           {filtered.map(
             ({ name, numericCode, flag, population, region, capital }) => (
-              <div className="col-12 col-md-6 col-xl-3 d-flex justify-content-center ">
-                <Link className="link" key={numericCode} to={`/${name}`}>
-                  <div className="card shadow  mb-5 bg-white rounded">
-                    <div className="card-image">
-                      <img className="image" src={flag} alt="flag" />
+              <div className='col-12 col-md-6 col-xl-3 d-flex justify-content-center '>
+                <Link className='link' key={numericCode} to={`/${name}`}>
+                  <div className='card shadow  mb-5 bg-white rounded'>
+                    <div className='card-image'>
+                      <img className='image' src={flag} alt='flag' />
                     </div>
-                    <div className="card-body text-left">
-                      <h5 className="card-title mb-4">{name}</h5>
-                      <p className="card-text">
+                    <div className='card-body text-left'>
+                      <h5 className='card-title mb-4'>{name}</h5>
+                      <p className='card-text'>
                         <span>Population:</span> {population}
                       </p>
-                      <p className="card-text">
+                      <p className='card-text'>
                         <span>Region:</span> {region}
                       </p>
-                      <p className="card-text">
+                      <p className='card-text'>
                         <span>Capital:</span> {capital}
                       </p>
                     </div>
@@ -38,24 +38,24 @@ function Card() {
           )}
         </div>
       ) : (
-        <div className="col-12 col-md-6 col-xl-12 border d-flex justify-content-center ">
+        <div className='container d-flex '>
           {countries.map(
             ({ name, numericCode, flag, population, region, capital }) => (
-              <div className="col-12 col-md-6 col-xl-12 d-flex justify-content-center ">
-                <Link className="link" key={numericCode} to={`/${name}`}>
-                  <div className="card shadow  mb-5 bg-white rounded">
-                    <div className="card-image">
-                      <img className="image" src={flag} alt="flag" />
+              <div className='col-12 col-md-6 col-xl-3  d-flex justify-content-center '>
+                <Link className='link' key={numericCode} to={`/${name}`}>
+                  <div className='card shadow  mb-5 bg-white rounded'>
+                    <div className='card-image'>
+                      <img className='image' src={flag} alt='flag' />
                     </div>
-                    <div className="card-body text-left">
-                      <h5 className="card-title mb-4">{name}</h5>
-                      <p className="card-text">
+                    <div className='card-body text-left'>
+                      <h5 className='card-title mb-4'>{name}</h5>
+                      <p className='card-text'>
                         <span>Population:</span> {population}
                       </p>
-                      <p className="card-text">
+                      <p className='card-text'>
                         <span>Region:</span> {region}
                       </p>
-                      <p className="card-text">
+                      <p className='card-text'>
                         <span>Capital:</span> {capital}
                       </p>
                     </div>
@@ -67,7 +67,7 @@ function Card() {
         </div>
       )}
     </>
-  );
+  )
 }
 
-export default Card;
+export default Card
