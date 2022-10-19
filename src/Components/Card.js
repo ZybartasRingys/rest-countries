5cd4cf5a329671f1b94c3199edcda74f7a61a57f
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-10-19 10:37:34 +0300

    filter and search functionality final

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,48 +1,52 @@
-import React from "react";
-import "../scss/card.scss";
-import { useContext } from "react";
-import { Context } from "../Context/StateContext";
-import { Link } from "react-router-dom";
+import React from 'react'
+import '../scss/card.scss'
+import { useContext } from 'react'
+import { Context } from '../Context/StateContext'
+import { Link } from 'react-router-dom'
 
 function Card() {
-  const { countries } = useContext(Context);
+  const { countries, noCountries } = useContext(Context)
 
   return (
     <>
-      <div className="container d-flex">
-        <div className="row">
-          {countries.map(
-            ({ name, numericCode, flag, population, region, capital }) => (
-              <div
-                className="col-12 col-md-6 col-xl-3 d-flex justify-content-center "
-                key={numericCode}
-              >
-                <Link className="link" to={`/${name}`}>
-                  <div className="card shadow  mb-5 bg-white rounded">
-                    <div className="card-image">
-                      <img className="image" src={flag} alt="flag" />
+      <div className='container d-flex'>
+        <div className='row'>
+          {!noCountries ? (
+            countries.map(
+              ({ name, numericCode, flag, population, region, capital }) => (
+                <div
+                  className='col-12 col-md-6 col-xl-3 d-flex justify-content-center '
+                  key={numericCode}
+                >
+                  <Link className='link' to={`/${name}`}>
+                    <div className='card shadow  mb-5 bg-white rounded'>
+                      <div className='card-image'>
+                        <img className='image' src={flag} alt='flag' />
+                      </div>
+                      <div className='card-body text-left'>
+                        <h5 className='card-title mb-4'>{name}</h5>
+                        <p className='card-text'>
+                          <span>Population:</span> {population}
+                        </p>
+                        <p className='card-text'>
+                          <span>Region:</span> {region}
+                        </p>
+                        <p className='card-text'>
+                          <span>Capital:</span> {capital}
+                        </p>
+                      </div>
                     </div>
-                    <div className="card-body text-left">
-                      <h5 className="card-title mb-4">{name}</h5>
-                      <p className="card-text">
-                        <span>Population:</span> {population}
-                      </p>
-                      <p className="card-text">
-                        <span>Region:</span> {region}
-                      </p>
-                      <p className="card-text">
-                        <span>Capital:</span> {capital}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-              </div>
+                  </Link>
+                </div>
+              )
             )
+          ) : (
+            <p>No countries found...</p>
           )}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Card;
+export default Card
--- src/Context/StateContext.js
+++ src/Context/StateContext.js
@@ -1,60 +1,79 @@
-import React, { createContext, useState, useEffect, useRef } from "react";
-import axios from "axios";
+import React, { createContext, useState, useEffect, useRef } from 'react'
+import axios from 'axios'
 
-export const Context = createContext();
+export const Context = createContext()
 
 export const ContextProvider = ({ children }) => {
-  const [countries, setCountries] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-  const [filteredCountryRegion, setFilteredCountryRegion] = useState("All");
-  const inputRef = useRef();
-  const regionRef = useRef();
+  const [countries, setCountries] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [filteredCountryRegion, setFilteredCountryRegion] = useState('All')
+  const inputRef = useRef()
+  const regionRef = useRef()
 
   /*  Country data endpoint  */
-  const baseURL = "https://restcountries.com/v2/all";
+  const baseURL = 'https://restcountries.com/v2/all'
+
+  const noCountries = countries.status || countries.message
 
   /* A hook that is used to fetch data. */
   useEffect(() => {
+    fetchCountries()
+  }, [])
+
+  const fetchCountries = () => {
     axios.get(baseURL).then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
+      setCountries(response.data)
+    })
+  }
 
   const searchCountries = () => {
-    const searchValue = inputRef.current.value;
+    const searchValue = inputRef.current.value
     if (searchValue.trim()) {
       const fetchSearch = async () => {
         const response = await fetch(
           `https://restcountries.com/v2/name/${searchValue}`
-        );
-        const data = await response.json();
-        setCountries(data);
-      };
-      try {
-        fetchSearch();
-      } catch (error) {
-        console.log(error);
+        )
+        const data = await response.json()
+        setCountries(data)
       }
+      try {
+        fetchSearch()
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      fetchCountries()
     }
-  };
+  }
 
   const filteredByRegion = () => {
-    const selectValue = regionRef.current.value;
+    const selectValue = regionRef.current.value
     if (selectValue.trim()) {
       const fetchSelect = async () => {
         const response = await fetch(
           `https://restcountries.com/v2/region/${selectValue}`
-        );
-        const data = await response.json();
+        )
+        const data = await response.json()
 
-        if (selectValue === "All") {
-          return countries;
-        } else {
-          return data;
+        if (selectValue === 'All') {
+          try {
+            fetchCountries()
+          } catch (error) {
+            console.log(error)
+          }
+          return
         }
-      };
+
+        setCountries(data)
+      }
+
+      try {
+        fetchSelect()
+      } catch (error) {
+        console.log(error)
+      }
     }
-  };
+  }
 
   return (
     <Context.Provider
@@ -69,9 +88,10 @@ export const ContextProvider = ({ children }) => {
         filteredByRegion,
         regionRef,
         inputRef,
+        noCountries,
       }}
     >
       {children}
     </Context.Provider>
-  );
-};
+  )
+}
