5c234a712df2a9d2d35f9df1aa9dadd6412c1c43
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-10-22 15:05:31 +0300

    breakpoints final

--- src/Components/CardDetail.jsx
+++ src/Components/CardDetail.jsx
@@ -29,7 +29,7 @@ function CardDetail() {
         if (id === name) {
           return (
             <div
-              className="container-fluid card-detail-container  "
+              className="container-fluid detail-card-container"
               key={numericCode}
             >
               <div className="row d-flex  justify-content-center">
@@ -49,10 +49,10 @@ function CardDetail() {
                       />
                     </div>
                   </div>
-                  <div className="col-12 col-md-11 col-xxl-5 px-3 d-flex flex-column  ">
+                  <div className="col-12 col-md-11 col-xxl-5 px-3 d-flex flex-column detail-card-text ">
                     <div className="col-12 col-md-6 col-xxl-10  d-flex flex-column flex-md-row align-items-md-center align-items-xl-center ">
-                      <div className="col-8 col-md-8 col-xxl-7">
-                        <h2 className="mb-4 ">{name}</h2>
+                      <div className="col-8 col-md-8 col-xxl-7 ">
+                        <h2 className="mb-4 country-name ">{name}</h2>
                         <p>
                           <span className="bold">Native name:</span>
                           {nativeName}
@@ -74,7 +74,7 @@ function CardDetail() {
                           {capital}
                         </p>
                       </div>
-                      <div className="col-12 col-md-4 col-xxl-6 mt-4 ">
+                      <div className="col-12 col-md-6 col-xxl-6  top-level">
                         <p>
                           <span className="bold">Top Level Domain:</span>
                           {topLevelDomain}
@@ -91,22 +91,24 @@ function CardDetail() {
                         </div>
                       </div>
                     </div>
-                    <div className="col-12 col-md-12 mt-4  ">
-                      <div className="col-12 col-md-12 ">
+                    <div className="col-6 col-md-12 mt-4 d-flex flex-column flex-xl-row">
+                      <div className="col-12 col-md-12 p-1 col-xl-4 ">
                         <h2 className="borders-heading">Border Co-untries:</h2>
                       </div>
 
-                      <div className="row d-flex mb-5 p-2   ">
-                        {borders &&
-                          borders.map((borderCountry) => {
-                            return (
-                              <div className="col-2 col-md-2 mb-2 border  link-container d-flex  justify-content-center align-items-center  border-link">
-                                <Link to={`/borders/${borderCountry}`}>
-                                  {borderCountry}
-                                </Link>
-                              </div>
-                            );
-                          })}
+                      <div className="row d-flex mb-5 p-1 ">
+                        <div className="col-12 col-md-12 col-xl-12 d-flex">
+                          {borders &&
+                            borders.map((borderCountry) => {
+                              return (
+                                <div className="col-5 col-md-2 col-xl-7 border mb-2 link-container d-flex  justify-content-center align-items-center  border-link">
+                                  <Link to={`/borders/${borderCountry}`}>
+                                    {borderCountry}
+                                  </Link>
+                                </div>
+                              );
+                            })}
+                        </div>
                       </div>
                     </div>
                   </div>
