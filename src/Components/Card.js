b38bb6f1a5fae1d40ebcec094cce9001ae0a3570
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-10-24 17:20:42 +0300

    breakpoints fixes Detail card comments

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,38 +1,38 @@
-import React from "react";
-import "../scss/card.scss";
-import { useContext } from "react";
-import { Context } from "../Context/StateContext";
-import { Link } from "react-router-dom";
+import React from 'react'
+import '../scss/card.scss'
+import { useContext } from 'react'
+import { Context } from '../Context/StateContext'
+import { Link } from 'react-router-dom'
 
 function Card() {
-  const { countries, noCountries } = useContext(Context);
+  const { countries, noCountries } = useContext(Context)
 
   return (
     <>
-      <div className="container">
-        <div className="row d-flex justify-content-center">
+      <div className='container-fluid'>
+        <div className='row d-flex justify-content-center'>
           {!noCountries ? (
             countries.map(
               ({ name, numericCode, flag, population, region, capital }) => (
                 <div
-                  className="col-12 col-md-6 col-xl-3 d-flex justify-content-center align-items-center"
+                  className='col-12 col-md-6 col-xl-3 d-flex justify-content-center align-items-center'
                   key={numericCode}
                 >
-                  <Link className="link" to={`/${name}`}>
-                    <div className="card country-card shadow  mb-5  rounded">
-                      <div className="card-image">
-                        <img className="image" src={flag} alt="flag" />
+                  <Link className='link' to={`/${name}`}>
+                    <div className='card country-card shadow  mb-5  rounded'>
+                      <div className='card-image'>
+                        <img className='image' src={flag} alt='flag' />
                       </div>
-                      <div className="card-body text-left mb-3">
-                        <h5 className="card-title mb-4 mt-2">{name}</h5>
-                        <p className="card-text">
-                          <span className="bold">Population:</span> {population}
+                      <div className='card-body text-left mb-3'>
+                        <h5 className='card-title mb-4 mt-2'>{name}</h5>
+                        <p className='card-text'>
+                          <span className='bold'>Population:</span> {population}
                         </p>
-                        <p className="card-text">
-                          <span className="bold">Region:</span> {region}
+                        <p className='card-text'>
+                          <span className='bold'>Region:</span> {region}
                         </p>
-                        <p className="card-text">
-                          <span className="bold">Capital:</span> {capital}
+                        <p className='card-text'>
+                          <span className='bold'>Capital:</span> {capital}
                         </p>
                       </div>
                     </div>
@@ -41,14 +41,14 @@ function Card() {
               )
             )
           ) : (
-            <div className="no-countries-container ">
-              <h3 className="no-countries">No countries found...</h3>
+            <div className='no-countries-container '>
+              <h3 className='no-countries'>No countries found...</h3>
             </div>
           )}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Card;
+export default Card
